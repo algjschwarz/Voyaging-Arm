0870fcdbdc5187d8d2b845534dc38ed0ddef1199
Alex Three <algjschwarz@gmail.com>
2026-09-06 14:03:40 -0500

    First Movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include <Servo.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist_pitch;
+Servo wrist_roll;
+Servo gripper;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Servo arr[] =
+  {base, shoulder, elbow, wrist_pitch, wrist_roll, gripper};
+  base.attach(3);
+  shoulder.attach(5);
+  elbow.attach(6);
+  wrist_pitch.attach(9);
+  wrist_roll.attach(10);
+  gripper.attach(11);
+  for (int i = 0; i < 6; i++) {
+    arr[i].write(90);
+  }
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
